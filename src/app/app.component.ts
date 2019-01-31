01c857e4baaf1fb73ac3f6bad96e2e79cc1b06e1
= <nncl@live.com>
2019-01-30 23:11:37 -0200

    Testing binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-dynamic-arr';
+    worktimes: any[];
+
+    constructor() {
+        this.worktimes = [];
+        this.buildWorktimes();
+    }
+
+    buildWorktimes(): void {
+        const weekdays = [
+            {id: 1, name: 'Monday', times: []},
+            {id: 2, name: 'Tuesday', times: []},
+            {id: 3, name: 'Wednesday', times: []},
+            {id: 4, name: 'Thursday', times: []},
+            {id: 5, name: 'Friday', times: []},
+            {id: 6, name: 'Saturday', times: []},
+            {id: 0, name: 'Sunday', times: []},
+            {id: 7, name: 'Holidays', times: []}
+        ];
+
+        weekdays.map((item) => {
+            const weektime = item;
+            this.worktimes.push(weektime);
+        });
+    }
+
+    addTime(item: object): void {
+        const time = {
+            start: null,
+            end: null
+        };
+
+        item.times.push(time);
+    }
 }
